134d1034cba74da4ce1251b8d6ab3c3726ac756c
smhrd <smhrd@DESKTOP-0JM7AHT>
2022-09-02 16:44:11 +0900

    프로젝트용 클래스 생성

---
+++ gittest/src/Controller/D_Con.java
@@ -0,0 +1,5 @@
+package Controller;
+
+public class D_Con {
+
+}
--- gittest/src/gittest/Test3.java
+++ gittest/src/gittest/Test3.java
@@ -4,6 +4,7 @@ public class Test3 {
 
 	public static void main(String[] args) {
 		System.out.println("다마다마");
+		System.out.println("고치고치");
 	}
 
 }
