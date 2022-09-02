bdc7920f7fdce73b0e71bc0aa6e2804ca2055b45
smhrd <smhrd@DESKTOP-EQR7VG7>
2022-09-02 15:38:37 +0900

    민성조,sleep명령어 추가했습니다.

--- gittest/src/gittest/Test.java
+++ gittest/src/gittest/Test.java
@@ -5,7 +5,14 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("First Commit!");
-		
+		try {
+			for (int i = 0; i < 2; i++) {
+				Thread.sleep(2000); // 1000 단위당 1초를 의미
+				System.out.println("Sleep "+i);
+			}
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 
 }
