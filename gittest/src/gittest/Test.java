bafd641f8ca8177109d59c4a41211f49dbb53420
smhrd <smhrd@DESKTOP-EQR7VG7>
2022-09-02 15:41:54 +0900

    두 번째 커밋/민성조

--- gittest/src/gittest/Test.java
+++ gittest/src/gittest/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("First Commit!");
+		System.out.println("Second Commit!");
 		try {
 			for (int i = 0; i < 2; i++) {
 				Thread.sleep(2000); // 1000 단위당 1초를 의미
